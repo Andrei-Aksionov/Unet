7e58c4a37d9fdeb10555786cf2d6bfa5e122b524
Andrei Aksionau <aksionau.andrei@gmail.com>
2021-09-04 16:15:46 +0300

    Raw implementation of UNET model

---
+++ model.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNET(nn.Module):
+    def __init__(self, in_channels: int = 3, out_channels: int = 1, features: list = [64, 128, 256, 512]) -> None:
+        super().__init__()
+        self.downs = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down part
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels=in_channels, out_channels=feature))
+            in_channels = feature
+
+        # Up part
+        for feature in features[::-1]:
+            self.ups.append(nn.ConvTranspose2d(in_channels=feature * 2, out_channels=feature, kernel_size=2, stride=2))
+            self.ups.append(
+                DoubleConv(in_channels=feature * 2, out_channels=feature),
+            )
+
+        self.bottleneck = DoubleConv(in_channels=features[-1], out_channels=features[-1] * 2)
+        self.final_conv = nn.Conv2d(in_channels=features[0], out_channels=out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx // 2]
+
+			# tensor with odd size (e.g. 161*161) after maxpool will have
+			# even size (80*80), so concatination might fail. For that reason
+			# resize is performed
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx + 1](concat_skip)
+
+        return self.final_conv(x)
+
+
+def test():
+    x = torch.randn((3, 1, 161, 161))
+    model = UNET(in_channels=1, out_channels=1)
+    preds = model(x)
+    assert preds.shape == x.shape
+
+
+if __name__ == "__main__":
+    test()
